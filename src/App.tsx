1428b3e84bef99087ceda31f8cc9d62975cd78c1
Ezequiel Almarone <ezequiel.almarone@gmail.com>
2021-06-22 17:41:25 -0400

    Adicionando o styled components e exemplificaçao

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import styled from 'styled-components';
 
+const Title = styled.h1`
+font-size: 66px;
+color: #8257e5;
+`
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World!!</h1>
+      <Title>Hello World!!</Title>
     </div>
   );
 }
